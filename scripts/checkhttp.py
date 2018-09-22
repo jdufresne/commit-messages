662e35b1e022146ffef9bfa4320de77e2a460601
Jon Dufresne <jon.dufresne@gmail.com>
2018-09-22 16:30:03 -0700

    Improve checkhttp script

--- scripts/checkhttp.py
+++ scripts/checkhttp.py
@@ -7,15 +7,31 @@ import subprocess
 import urllib
 
 
-url_re = re.compile(r'\bhttp://[^ ,"\')<>\]]+')
+# From Django's URLValidator.
+ul = '\u00a1-\uffff'  # unicode letters range (must not be a raw string)
+
+# Host patterns
+hostname_re = r'[a-z' + ul + r'0-9](?:[a-z' + ul + r'0-9-]{0,61}[a-z' + ul + r'0-9])?'
+# Max length for domain name labels is 63 characters per RFC 1034 sec. 3.1
+domain_re = r'(?:\.(?!-)[a-z' + ul + r'0-9-]{1,63}(?<!-))*'
+tld_re = (
+    r'\.'                                # dot
+    r'(?!-)'                             # can't start with a dash
+    r'(?:[a-z' + ul + '-]{2,63}'         # domain label
+    r'|xn--[a-z0-9]{1,59})'              # or punycode label
+    r'(?<!-)'                            # can't end with a dash
+    r'\.?'                               # may have a trailing dot
+)
+host_re = hostname_re + domain_re + tld_re
+
+url_re = re.compile(
+    r'http://' + host_re + r'(?:[/?#][^\s]*)?',
+    re.IGNORECASE
+)
 example_re = re.compile(r'\bexample\.')
 
 
 def main():
-    # prune_wholepath = [
-    #     'data',
-    # ]
-
     prune_name = [
         '.git',
         '.hg',
@@ -48,6 +64,7 @@ def main():
         '.pyo',
         '.sql',
         '.svg',
+        '.svg',
         '.tgz',
         '.ttf',
         '.whl',
@@ -59,14 +76,8 @@ def main():
     ]
 
     root = os.getcwd()
+    urls = {}
     for dirpath, dirnames, filenames in os.walk(root):
-        # if dirpath == root:
-        #     for name in prune_wholepath:
-        #         try:
-        #             dirnames.remove(name)
-        #         except ValueError:
-        #             pass
-
         for name in prune_name:
             try:
                 dirnames.remove(name)
@@ -74,7 +85,6 @@ def main():
                 pass
 
         for fn in filenames:
-
             _, ext = os.path.splitext(fn)
             if ext not in ignore_ext:
                 path = os.path.join(dirpath, fn)
@@ -83,56 +93,59 @@ def main():
 
                 print(path)
 
-                urls = {}
                 try:
                     with open(path) as fp:
                         for line in fp:
                             matches = url_re.findall(line)
                             for match in matches:
-                                match = match.strip()
+                                # Strip rst & md syntax.
+                                match = match.rstrip('"\'(),.<>[]_`')
                                 if match not in urls:
-                                    if '.' in match:
-                                        if not example_re.search(match):
-                                            print('  %s' % match)
+                                    pr = list(urllib.parse.urlparse(match))
+                                    if not example_re.search(pr[1]):
+                                        print('  %s' % match)
 
-                                            pr = list(urllib.parse.urlparse(match))
-                                            pr[0] = 'https'
-                                            secure_url = urllib.parse.urlunparse(pr)
+                                        pr[0] = 'https'
+                                        secure_url = urllib.parse.urlunparse(pr)
 
-                                            urls[match] = None
-                                            try:
-                                                r = requests.get(secure_url, timeout=3)
-                                            except (requests.exceptions.SSLError,
-                                                    requests.exceptions.ConnectionError,
-                                                    requests.exceptions.ReadTimeout,
-                                                    requests.exceptions.TooManyRedirects):
-                                                pass
-                                            else:
-                                                pr = urllib.parse.urlparse(r.url)
-                                                if pr.scheme == 'https' and r.status_code == 200:
-                                                    print('  -> %s' % r.url)
-                                                    urls[match] = r.url
+                                        urls[match] = None
+                                        try:
+                                            r = requests.get(secure_url, timeout=3)
+                                        except requests.exceptions.InvalidURL as e:
+                                            print('    %s' % e)
+                                        except (requests.exceptions.SSLError,
+                                                requests.exceptions.ConnectionError,
+                                                requests.exceptions.ReadTimeout,
+                                                requests.exceptions.TooManyRedirects):
+                                            pass
+                                        else:
+                                            pr = urllib.parse.urlparse(r.url)
+                                            if pr.scheme == 'https' and r.status_code == 200:
+                                                print('  -> %s' % r.url)
+                                                urls[match] = r.url
                 except UnicodeDecodeError as e:
                     print(str(e))
 
-                for old_url, new_url in urls.items():
-                    if new_url:
-                        cmd = [
-                            'find', root,
+    for old_url, new_url in sorted(urls.items(), key=lambda item: len(item[0]), reverse=True):
+        if new_url:
+            print(f'{old_url} -> {new_url}')
 
-                            '-name', '.git', '-prune', '-o',
-                            '-name', '.hg', '-prune', '-o',
-                            '-name', '.tox', '-prune', '-o',
-                            '-name', '__pycache__', '-prune', '-o',
-                            '-name', 'LC_MESSAGES', '-prune', '-o',
-                            '-name', 'node_modules', '-prune', '-o',
-                            '-type', 'f',
+            prune_args = []
+            for name in prune_name:
+                prune_args.extend(['-name', name, '-prune', '-o'])
 
-                            '-exec', 'sed', '-i', '-e', 's|%s|%s|g' % (re.escape(old_url), new_url), '{}', ';'
-                        ]
-                        subprocess.check_call(cmd)
+            ignore_args = []
+            for ext in ignore_ext:
+                ignore_args.extend(['-not', '-name', f'*{ext}'])
 
-
+            cmd = [
+                'find', root,
+                *prune_args,
+                *ignore_args,
+                '-type', 'f',
+                '-exec', 'sed', '-i', '-e', 's|%s|%s|g' % (re.escape(old_url), new_url), '{}', ';'
+            ]
+            subprocess.check_call(cmd)
 
 
 if __name__ == '__main__':
